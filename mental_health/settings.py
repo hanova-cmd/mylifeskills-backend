024e3e8e0ce2b0bf49ba0ac2de12c389b0e95d2b
hanova-cmd <nazerkeaitahanova@gmail.com>
2026-06-27 01:47:40 +0500

    Fix database connection settings

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -14,7 +14,7 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'), 
+        default=os.environ.get('sqlite:///db.sqlite3'), 
         conn_max_age=600
     )
 }
